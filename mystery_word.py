9f1c4875f513b2ab10c97d7dc46783bc71994fd3
Luke Wheeler <102833183+GitLukeW@users.noreply.github.com>
2022-08-15 15:46:27 -0400

    game is working still need to add items from the readme

--- mystery_word.py
+++ mystery_word.py
@@ -1,6 +1,53 @@
+import random
+
+
 def play_game():
-    pass
+    filepath = open("test-word.txt", "r")
+    open_file = filepath.read()
+    print(open_file)
+
+    words = open_file.split()
+    counter = 8
+
+    random_word = random.choice(words)
+    print(random_word)
+
+    blank = [" __ "] * len(random_word)
+
+    print(blank)
+
+    random_word_picked = random_word
+
+    letters = list(random_word_picked)
+    print(letters)
+
+    # guess = input("pick a letter:")
+
+    user_letters(blank, letters, counter)
 
 
+def user_letters(blank, letters, counter):
+    guess = input("pick a letter:")
+    if guess in letters:
+        for i in range(len(letters)):
+            if guess == letters[i]:
+                blank[i] = letters[i]
+        print(blank)
+        if " __ " not in blank:
+            print("You Win!")
+            exit()
+        if counter > 0:
+            user_letters(blank, letters, counter)
+
+    else:
+        counter -= 1
+        if counter > 0:
+            user_letters(blank, letters, counter)
+        else:
+            print("Game Over")
+            exit()
+
+
+# def guess_counter():
 if __name__ == "__main__":
     play_game()
